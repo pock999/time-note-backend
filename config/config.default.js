446afa1f9109606a5e542b9f27c23fb52a29f042
Rickh <hpx210650@gmail.com>
2022-04-20 02:59:34 +0800

    🎉 init

---
+++ config/config.default.js
@@ -0,0 +1,26 @@
+module.exports = {
+  port: 3000,
+  bootstrapData: false,
+
+  database: {
+    host: 'localhost',
+    dialect: 'mariadb',
+    name: 'time-note',
+    username: 'root',
+    password: '',
+    dialectOptions: {
+      charset: 'utf8mb4',
+      useUTC: false, //for reading from database
+    },
+    sync: {
+      force: true,
+    },
+    logging: false,
+    timezone: '+08:00', //for writing to database
+  },
+
+  jwt: {
+    secret: 'secret',
+    expiresIn: '15m', // 15分
+  },
+};
