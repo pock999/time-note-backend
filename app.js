f7180c844b9b04c30d3c8d2704da20f2cd804c45
Rickh <hpx210650@gmail.com>
2022-04-22 00:23:01 +0800

    ✨ feat: add global variable

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./api/routes/index');
-var usersRouter = require('./api/routes/users');
+const indexRouter = require('./api/routes/index');
+const usersRouter = require('./api/routes/users');
 
-var app = express();
+const app = express();
+
+const dbModels = require('./api/models');
+// config
+const config = require('./config/config');
+
+// global
+const dayjs = require('dayjs');
+const _ = require('lodash');
+
+global.dayjs = dayjs;
+global._ = _;
+global.JsonReParse = (obj) => JSON.parse(JSON.stringify(obj));
+global.JsonSerialize = (obj) => JSON.stringify(obj);
+global.JsonParse = (obj) => JSON.stringify(obj);
+global.config = config;
+
+dbModels.sequelize
+  .sync(config.database.sync)
+  .then(async () => {})
+  .catch((err) => {
+    console.log('error => ', err);
+  });
+
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
