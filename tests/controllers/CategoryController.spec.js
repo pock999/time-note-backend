7082cb2a86f3999eb83a639616ce81d98de9690b
Rickh <hpx210650@gmail.com>
2022-08-17 03:03:35 +0800

    🧪 test: CategoryController.Detail

--- tests/controllers/CategoryController.spec.js
+++ tests/controllers/CategoryController.spec.js
@@ -74,3 +74,80 @@ describe('=== 列表Category - GET /category/list ===', async () => {
     expect(res.body.message).to.be.equal('success');
   });
 });
+
+describe('=== 單一Category - GET /category/:id ===', async () => {
+  const now = dayjs();
+
+  const userData = {
+    name: '王小明',
+    email: 'ming1234@google.com',
+    password: 'abcd1234',
+  };
+
+  const categorysData = [
+    {
+      name: '重要',
+      color: '#F83637',
+    },
+    {
+      name: '待辦',
+      color: '#AADFC9',
+    },
+    {
+      name: '閱讀清單',
+      color: '#84C9E2',
+    },
+  ];
+
+  beforeEach(async () => {
+    await dbModels.sequelize.sync({ force: true, logging: false });
+
+    await dbModels.User.destroy({ where: {}, truncate: true });
+
+    const user = await dbModels.User.create(userData);
+
+    await dbModels.Category.destroy({ where: {}, truncate: true });
+
+    await dbModels.Category.bulkCreate(
+      categorysData.map((item) => ({ ...item, UserId: user.id }))
+    );
+  });
+
+  afterEach(async function () {
+    await await dbModels.User.destroy({ where: {}, truncate: true });
+    await await dbModels.Category.destroy({ where: {}, truncate: true });
+  });
+
+  it('- 獲取成功', async () => {
+    const authorizationToken = await callLogin({
+      ..._.pick(userData, ['email', 'password']),
+    });
+
+    const res = await request(app)
+      .get('/category/1')
+      .set({
+        Authorization: `Bearer ${authorizationToken}`,
+      });
+
+    expect(res.statusCode).to.be.equal(200);
+    expect(res.body.message).to.be.equal('success');
+    expect(res.body.data.id).to.be.equal(1);
+    expect(res.body.data.name).to.be.equal(categorysData[0].name);
+    expect(res.body.data.color).to.be.equal(categorysData[0].color);
+  });
+
+  it('- 獲取失敗(不存在)', async () => {
+    const authorizationToken = await callLogin({
+      ..._.pick(userData, ['email', 'password']),
+    });
+
+    const res = await request(app)
+      .get('/category/1000')
+      .set({
+        Authorization: `Bearer ${authorizationToken}`,
+      });
+
+    expect(res.statusCode).to.be.equal(404);
+    expect(res.body.message).to.be.equal('NotFound.Target.Not.Found');
+  });
+});
