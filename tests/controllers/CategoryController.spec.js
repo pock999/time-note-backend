026a8f51d3c7a434f727a450c1b9492fe58e0c1c
Rickh <hpx210650@gmail.com>
2022-08-17 03:13:41 +0800

    🧪 test: CategoryController.Update

--- tests/controllers/CategoryController.spec.js
+++ tests/controllers/CategoryController.spec.js
@@ -228,3 +228,101 @@ describe('=== 新增Category - POST /category ===', async () => {
     expect(res.body.message).to.be.equal('BadRequest.Data.Duplicated');
   });
 });
+
+describe('=== 更新Category - PUT /category:id ===', async () => {
+  const userData = {
+    name: '王小明',
+    email: 'ming1234@google.com',
+    password: 'abcd1234',
+  };
+
+  const categorysData = [
+    {
+      name: '重要',
+      color: '#F83637',
+    },
+    {
+      name: '待辦',
+      color: '#AADFC9',
+    },
+    {
+      name: '閱讀清單',
+      color: '#84C9E2',
+    },
+  ];
+
+  beforeEach(async () => {
+    await dbModels.sequelize.sync({ force: true, logging: false });
+
+    await dbModels.User.destroy({ where: {}, truncate: true });
+
+    const user = await dbModels.User.create(userData);
+
+    await dbModels.Category.destroy({ where: {}, truncate: true });
+
+    await dbModels.Category.bulkCreate(
+      categorysData.map((item) => ({ ...item, UserId: user.id }))
+    );
+  });
+
+  afterEach(async function () {
+    await await dbModels.User.destroy({ where: {}, truncate: true });
+    await await dbModels.Category.destroy({ where: {}, truncate: true });
+  });
+
+  it('- 更新成功', async () => {
+    const authorizationToken = await callLogin({
+      ..._.pick(userData, ['email', 'password']),
+    });
+
+    const res = await request(app)
+      .put('/category/1')
+      .send({
+        name: '測試',
+        color: '#111111',
+      })
+      .set({
+        Authorization: `Bearer ${authorizationToken}`,
+      });
+
+    expect(res.statusCode).to.be.equal(200);
+    expect(res.body.message).to.be.equal('success');
+    expect(res.body.data.name).to.be.equal('測試');
+    expect(res.body.data.color).to.be.equal('#111111');
+  });
+
+  it('- 更新失敗(重複)', async () => {
+    const authorizationToken = await callLogin({
+      ..._.pick(userData, ['email', 'password']),
+    });
+
+    const res = await request(app)
+      .put('/category/1')
+      .send(categorysData[1])
+      .set({
+        Authorization: `Bearer ${authorizationToken}`,
+      });
+
+    expect(res.statusCode).to.be.equal(400);
+    expect(res.body.message).to.be.equal('BadRequest.Data.Duplicated');
+  });
+
+  it('- 更新失敗(不存在)', async () => {
+    const authorizationToken = await callLogin({
+      ..._.pick(userData, ['email', 'password']),
+    });
+
+    const res = await request(app)
+      .put('/category/1000')
+      .send({
+        name: '測試',
+        color: '#111111',
+      })
+      .set({
+        Authorization: `Bearer ${authorizationToken}`,
+      });
+
+    expect(res.statusCode).to.be.equal(404);
+    expect(res.body.message).to.be.equal('NotFound.Target.Not.Found');
+  });
+});
